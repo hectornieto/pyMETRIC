dd71f1032a7b4fb21a2c8fd7654869ccdfd0f150
hectornieto <hector.nieto.solana@gmail.com>
2018-11-01 19:02:14 +0100

    Aestetics changes in documentation

--- pyMETRIC/METRIC.py
+++ pyMETRIC/METRIC.py
@@ -267,7 +267,7 @@ def METRIC(Tr_K,
     dT_a = (dT_endmembers[1] - dT_endmembers[0]) \
             / (Tr_datum[hot_pixel] - Tr_datum[cold_pixel])    #Allen 2007 eq 50
     
-    dT_b = (dT_endmembers[1] - dT_a) / Tr_datum[hot_pixel]                    #Allen 2007 eq 51
+    dT_b = (dT_endmembers[1] - dT_a) / Tr_datum[hot_pixel]    #Allen 2007 eq 51
     
     #Apply the constant to the whole image
     dT = dT_a + dT_b * Tr_datum                         #Allen 2007 eq. 29
